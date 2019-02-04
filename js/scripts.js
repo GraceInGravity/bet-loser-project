1e0caa542a8b48cb38f8a13a933b19c855ed2f16
Student <example@example.com>
2019-02-04 13:53:06 -0800

    it will have initial form html with submit and display html based on inputs

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,33 @@ function User(userName, userEmail, userBank) {
   this.userEmail = userEmail;
   this.userBank = userBank;
 }
+
+Bet.prototype.getDisplayHTML = function () {
+  var html  = "<h1>" + this.betName  + "</h1> \
+  <p>" + this.betUser + " is involved in this bet for " + this.betTerms;
+
+  return html;
+};
+
+
+$(function(){
+  $("#bet-form").submit(function(event){
+    console.log("hello from submit button");
+    event.preventDefault();
+    var betName = $("input[name='bet-name']").val();
+    var betUser1 = $("input[name='user1']").val();
+    var betUser1Email = $("input[name='email1']").val();
+    var betUser2 = $("input[name='user2']").val();
+    var betUser2Email = $("input[name='email2']").val();
+    var betNotes = $("textarea[name='bet-notes']").val();
+    var betPenalty = "Bet Penalty Goes Here";
+
+    var newBet = new Bet(betName, betUser1, betNotes, betPenalty, false);
+
+    $(".bets").html(newBet.getDisplayHTML());
+
+
+
+    var currentBet = new Bet()
+  })
+});
